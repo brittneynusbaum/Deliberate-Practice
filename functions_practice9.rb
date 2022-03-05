8397fe2b4f4cfb77ef57ee7f1ae5ed5c9656446d
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-04 21:29:50 -0800

    adds functions 9

--- functions_practice9.rb
+++ functions_practice9.rb
@@ -1,1 +1,7 @@
 # Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+def average(num_1, num_2, num_3)
+  answer = (num_1 + num_2 + num_3)/3.0
+end
+
+p average(2, 4, 6)
