771a0f19170aba6d84a79b7a00011bcc8a775744
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-04 21:08:11 -0800

    adds function 5

---
+++ functions_practice5.rb
@@ -0,0 +1,7 @@
+# Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+def first_letter(string)
+  string = string[0]
+end
+
+p first_letter("hello")
