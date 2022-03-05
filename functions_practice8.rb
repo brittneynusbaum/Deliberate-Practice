10706115a56419f24edcacb9a57ad43e5bfbe4b1
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-04 21:25:41 -0800

    adds functions 8

--- functions_practice8.rb
+++ functions_practice8.rb
@@ -1,1 +1,9 @@
 # Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+def repeat(string)
+  5.times do
+    p string 
+  end
+end
+
+repeat("Hey!")
