6cc5a0d0990f15ebb138277529f970b62175f22b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-04 21:03:13 -0800

    adds functions 4

---
+++ functions_practice4.rb
@@ -0,0 +1,7 @@
+# Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+def multiply(number)
+  number *= number
+end
+
+p multiply(5)
