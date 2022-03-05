48b23e6700912a7c0d9983b98a8a3d0a8ce00f7e
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-04 21:22:29 -0800

    adds functions 7

--- functions_practice7.rb
+++ functions_practice7.rb
@@ -1,1 +1,7 @@
 # Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def convert(number)
+  number = number.to_s
+end
+
+p convert(578)
