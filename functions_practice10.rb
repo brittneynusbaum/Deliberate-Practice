ee6b60a76cf277ace69e9a9b54d0222e8776419f
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-04 21:32:32 -0800

    adds functions 10

--- functions_practice10.rb
+++ functions_practice10.rb
@@ -1,1 +1,7 @@
 # Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def times_plus(number) 
+  number = (number * 10) + 30
+end
+
+p times_plus(10)
