be2d80376c492bdc84c698341bf2e68da0741d6c
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 15:50:50 -0800

    adds conditional practice 4 solution

--- conditionals/conditionals_practice4.rb
+++ conditionals/conditionals_practice4.rb
@@ -1,1 +1,10 @@
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+
+x = 8999
+
+if x > 9000
+  p 1
+else
+  p -1
+end
