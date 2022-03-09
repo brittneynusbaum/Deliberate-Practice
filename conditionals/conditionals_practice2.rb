0f95ff61ed808e6410c40f9843e3d9e06091c857
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 15:45:36 -0800

    adds conditional practice 2 solution

--- conditionals/conditionals_practice2.rb
+++ conditionals/conditionals_practice2.rb
@@ -1,1 +1,11 @@
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+x = 11
+
+if x < 10
+  p -1
+elsif x > 10
+  p 1
+elsif x ==10
+  p 0
+end
