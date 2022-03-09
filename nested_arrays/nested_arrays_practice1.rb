9aa8a1ea8702230604ae5dc2cfb961953bcd6a57
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 14:14:50 -0800

    adds conditionals

--- nested_arrays/nested_arrays_practice1.rb
+++ nested_arrays/nested_arrays_practice1.rb
@@ -1,3 +1,31 @@
 #  1. Use a nested loop to convert an array of number pairs into a single flattened array.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
 
+numbers = [[1, 3], [8, 9], [2, 16]]
+
+i = 0
+a = 0
+new_array = []
+
+numbers.each do |number|
+  arrays.each do |array|
+    new_array << numbers[i][a]
+    a += 1
+  end
+  i += 1
+  a = 0
+end
+
+p new_array
+
+ 
+# new_array << array
+
+# i = 0
+# new_array = []
+# numbers.each do |number|
+#   array = numbers[i]
+#   new_array << array
+# end
+
+# p new_array
--- nested_arrays/nested_arrays_practice2.rb
+++ nested_arrays/nested_arrays_practice2.rb
@@ -1,2 +1,6 @@
 #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
+
+
+array1 = ["a", "b", "c"]
+array2 = ["d", "e", "f", "g"]
