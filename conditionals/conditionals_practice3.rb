66b6e0dc44209726266068b7ee060102772c501c
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 15:47:41 -0800

    adds conditional practice 3 solution

--- conditionals/conditionals_practice3.rb
+++ conditionals/conditionals_practice3.rb
@@ -1,1 +1,10 @@
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+x = 12
+y = 11
+
+if x < 10 && y < 10
+  p 1
+else
+  p 0
+end
