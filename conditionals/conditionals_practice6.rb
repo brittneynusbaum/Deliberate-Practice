d8d712b3ca5184038c854a859e7d5f1c309fdfb8
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 16:04:02 -0800

    adds conditional solution 6 solution

--- conditionals/conditionals_practice6.rb
+++ conditionals/conditionals_practice6.rb
@@ -1,1 +1,10 @@
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+x = 80
+y = 6
+
+if x > 10 || y > 10
+  p 100
+else
+  p -100
+end
