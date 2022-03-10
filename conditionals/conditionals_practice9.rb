923e94765c214357c7b057f941373876e78effef
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 16:10:31 -0800

    adds conditional practice 9 solution

--- conditionals/conditionals_practice9.rb
+++ conditionals/conditionals_practice9.rb
@@ -1,1 +1,10 @@
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+x = 6
+y = -1
+
+if x < 0 && y > 0
+  p 1
+else 
+  p 0
+end
