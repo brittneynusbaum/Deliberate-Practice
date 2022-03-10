a8f908d91fee788688d037dee00e339f028e4280
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-09 16:00:42 -0800

    adds conditional practice 5 solution

--- conditionals/conditionals_practice5.rb
+++ conditionals/conditionals_practice5.rb
@@ -1,1 +1,13 @@
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+x = 77
+
+if x < 10
+  p 9
+elsif x > 10 && x < 20
+  p 19
+elsif x > 20 && x < 30
+  p 29
+else
+  p -1
+end
