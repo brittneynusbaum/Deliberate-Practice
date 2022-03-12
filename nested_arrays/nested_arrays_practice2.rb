ca7f8b6cf1abfb3375627512309828dd3a61b00e
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-12 15:42:41 -0800

    adds practice 2 solution

--- nested_arrays/nested_arrays_practice2.rb
+++ nested_arrays/nested_arrays_practice2.rb
@@ -4,3 +4,17 @@
 
 array1 = ["a", "b", "c"]
 array2 = ["d", "e", "f", "g"]
+
+combined = []
+i = 0
+while i < array1.length
+  a = 0
+  while a < array2.length
+    new_string = array1[i] + array2[a]
+    combined << new_string
+    a += 1
+  end
+  i += 1
+end
+
+p combined
