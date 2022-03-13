d80c6d9effff0d3b758f47dbb0cc41066400983c
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-12 20:04:30 -0800

    adds practice 5 solution

--- nested_arrays/nested_arrays_practice4.rb
+++ nested_arrays/nested_arrays_practice4.rb
@@ -1,2 +1,20 @@
 #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
 #     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+
+
+array = [5, -2, 1, -9, -7, 2, 6]
+product = []
+i = 0
+while i < array.length
+  a = 0
+  while a < array.length
+    if array[i] != array[a]
+      product << array[i] * array[a]
+    end
+    a += 1
+  end
+  i += 1
+end
+
+answer = product.sort
+p answer[-1]
--- nested_arrays/nested_arrays_practice5.rb
+++ nested_arrays/nested_arrays_practice5.rb
@@ -1,2 +1,18 @@
 #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
+
+numbers = [[1, 3], [8, 9], [2, 16]]
+i = 0
+sum = 0
+while i < numbers.length
+  nested_array = numbers[i]
+  a = 0
+  while a < nested_array.length
+    number_array = nested_array[a]
+    sum = sum + number_array
+    a += 1
+  end
+  i += 1
+end
+
+p sum
