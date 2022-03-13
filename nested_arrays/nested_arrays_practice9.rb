8d00ad319933ac6536bf95e350686b6c577f35b9
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-12 21:11:14 -0800

    adds practice 9 solution

--- nested_arrays/nested_arrays_practice9.rb
+++ nested_arrays/nested_arrays_practice9.rb
@@ -1,2 +1,21 @@
 #  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
 #     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+
+array = [2, 5, 7, 1, 0, 7, 11] 
+sum_ten = []
+i = 0
+while i < array.length
+  a = 0
+  while a < array.length
+    if array[i] + array[a] == 10 && array[i] != array[a]
+      result = sum_ten << array[i] 
+      if array[i] + array[a] != 10
+        result = false ## printing nil, not 100% working
+      end
+    end
+    a += 1
+  end
+  i += 1
+end
+
+p result
