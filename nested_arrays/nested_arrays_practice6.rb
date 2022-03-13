6e426992bc38db0c375317b41734dee7fa348995
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 14:05:56 -0700

    adds practice 6 round 2

--- nested_arrays/nested_arrays_practice6.rb
+++ nested_arrays/nested_arrays_practice6.rb
@@ -3,20 +3,20 @@
 
 ## Practice Round 2 ##
 
+array1 = [1, 2] 
+array2 = [6, 7, 8]
+sums = []
+i = 0
+while i < array1.length
+  a = 0
+  while a < array2.length
+    sums << array1[i] + array2[a]
+    a += 1
+  end
+  i += 1
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+p sums
 
 ## Practice Round 1 ##
 
