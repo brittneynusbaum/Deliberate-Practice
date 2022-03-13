6e1f5f59c483454437e41f0674fbdd9cfec97ce4
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 14:01:46 -0700

    adds practice 5 round 2

--- nested_arrays/nested_arrays_practice5.rb
+++ nested_arrays/nested_arrays_practice5.rb
@@ -3,20 +3,21 @@
 
 ## Practice Round 2 ##
 
+arrays = [[1, 3], [8, 9], [2, 16]]
+i = 0
+sum = 0   # a little confused on why we set sum to 0 first - looking into for clarity.
+while i < arrays.length
+  nested_array = arrays[i]
+  a = 0
+  while a < nested_array.length
+    nested_index = nested_array[a]
+    sum = sum + nested_index
+    a += 1 
+  end
+  i += 1
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+p sum
 
 ## Practice Round 1 ##
 
