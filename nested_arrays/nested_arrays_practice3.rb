20cc691d1471a8ae2b910bed5e5f1f3d84be4184
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-12 16:44:26 -0800

    adds practice 3 solution

--- nested_arrays/nested_arrays_practice3.rb
+++ nested_arrays/nested_arrays_practice3.rb
@@ -1,2 +1,20 @@
 #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+array = ["a", "b", "c", "d"]
+combination = []
+i = 0
+while i < array.length
+  a = 0
+  while a < array.length 
+    if array[i] != array[a]
+      combination << array[i] + array[a]
+      a += 1
+    elsif array[i] == array[a]
+      a += 1
+    end
+  end
+  i += 1
+end
+
+p combination
