7c022f5cd141935d41dae401b95b4519e9876673
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:17:41 -0700

    adds practice 6 solution

--- arrays_map/arrays_map_practice6.rb
+++ arrays_map/arrays_map_practice6.rb
@@ -1,2 +1,9 @@
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+array_hashes = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+age = []
+array_hashes.each do |array_hash|
+  age << array_hash[:age]
+end
+p age
