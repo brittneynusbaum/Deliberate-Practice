502f9bde094f3c18f813b1d07bab213032264846
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 18:52:07 -0700

    adds practice 2 solution

--- arrays_map/arrays_map_practice2.rb
+++ arrays_map/arrays_map_practice2.rb
@@ -1,2 +1,9 @@
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+strings = ["hello", "goodbye"]
+upcase = []
+strings.each do |string|
+  upcase << string.upcase
+end
+p upcase
