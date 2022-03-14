4c162aed451e3a5f45ab58e3685698f6db263978
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:40:40 -0700

    adds practice 10 solution

--- arrays_map/arrays_map_practice10.rb
+++ arrays_map/arrays_map_practice10.rb
@@ -1,2 +1,10 @@
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+numbers =  [1, 2, 3]
+string = []
+numbers.each do |number|
+  string << number.to_s
+end
+
+p string
