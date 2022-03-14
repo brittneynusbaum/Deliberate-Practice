bce3ca2eff1665b24a7149eaa8683be341cb947d
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:11:49 -0700

    adds practice 5 solution

--- arrays_map/arrays_map_practice5.rb
+++ arrays_map/arrays_map_practice5.rb
@@ -1,2 +1,9 @@
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
+
+strings = ["hello", "goodbye"]
+strings_length = []
+strings.each do |string|
+  strings_length << string.length
+end
+p strings_length
