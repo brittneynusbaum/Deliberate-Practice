ad0197acb6576bcd0d371bd65eff9244c0f0d280
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:36:31 -0700

    adds practice 9 solution

--- arrays_map/arrays_map_practice9.rb
+++ arrays_map/arrays_map_practice9.rb
@@ -1,2 +1,10 @@
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
+
+array_hashes = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+age_two = []
+array_hashes.each do |array_hash|
+  age_two << array_hash[:age] * 2
+end
+
+p age_two
