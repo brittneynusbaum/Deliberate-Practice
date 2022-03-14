bb1b0db082119a0238c29525e535eaaabfd910d0
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:32:38 -0700

    adds practice 8 solution

--- arrays_map/arrays_map_practice8.rb
+++ arrays_map/arrays_map_practice8.rb
@@ -1,2 +1,10 @@
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
+
+strings = ["hello", "goodbye"]
+first_letter = []
+strings.each do |string|
+  first_letter << string[0]
+end
+
+p first_letter
