ec140c579c26ef6b207289e08cf74f0e108d51f9
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 18:49:17 -0700

    adds practice 1 solution

--- arrays_map/arrays_map_practice1.rb
+++ arrays_map/arrays_map_practice1.rb
@@ -1,2 +1,10 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
+
+numbers = [1, 2, 3]
+times_three = []
+numbers.each do |number|
+  times_three << number * 3
+end
+
+p times_three
