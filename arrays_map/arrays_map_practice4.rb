00427cf7ceb195f337c77fc960561bdd879c21a9
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:07:21 -0700

    adds practice 4 solution

--- arrays_map/arrays_map_practice4.rb
+++ arrays_map/arrays_map_practice4.rb
@@ -1,2 +1,11 @@
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+numbers = [1, 2, 3]
+plus_seven = []
+
+numbers.each do |number|
+  plus_seven << number + 7
+end
+
+p plus_seven
