c40ad52370d5feb1940cb933c4959f6a4c274b1b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-13 19:29:40 -0700

    adds practice 7 solution

--- arrays_map/arrays_map_practice7.rb
+++ arrays_map/arrays_map_practice7.rb
@@ -1,2 +1,10 @@
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+numbers = [1, 2, 3]
+divided_two = []
+numbers.each do |number|
+  divided_two << number.to_f / 2
+end
+
+p divided_two
