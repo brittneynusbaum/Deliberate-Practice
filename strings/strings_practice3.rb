d60af1faf5338c628e0918acc8d82b44fc7eae70
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 20:47:30 -0700

    adds practice 3 solution

--- strings/strings_practice3.rb
+++ strings/strings_practice3.rb
@@ -1,1 +1,8 @@
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+p "Enter a word!"
+
+user_input = gets.chomp.downcase
+if user_input == "marco"
+  p "polo"
+end
