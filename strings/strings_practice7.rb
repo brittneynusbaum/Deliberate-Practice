491ec6e031847134eb31620ccb704b41a73fe52f
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 21:04:34 -0700

    adds practice 7 solution

--- strings/strings_practice7.rb
+++ strings/strings_practice7.rb
@@ -1,1 +1,8 @@
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+p "Enter a book title and author"
+
+book_title = gets.chomp
+author = gets.chomp
+
+p "The book is " + book_title + " and the author is " + author + "."
