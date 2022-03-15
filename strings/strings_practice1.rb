7f0a30bcef7104e8368b84b1fb5359e7d7ba1237
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 20:42:54 -0700

    edits practice 1 solution

--- strings/strings_practice1.rb
+++ strings/strings_practice1.rb
@@ -1,6 +1,5 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-p "Enter your first and last name one at a time."
 first_name = "Brittney"
 last_name = "Nusbaum"
 
