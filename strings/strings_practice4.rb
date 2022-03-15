d47ac4fa0f52b591fd5292042eafd547ce909b80
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 20:52:31 -0700

    adds practice 4 solution

--- strings/strings_practice4.rb
+++ strings/strings_practice4.rb
@@ -1,1 +1,8 @@
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+p "Enter three colors, one at a time."
+
+user_color1 = gets.chomp
+user_color2 = gets.chomp
+user_color3 = gets.chomp
+
+p "Your colors are " + user_color1 + ", " + user_color2 + ", and " + user_color3 + "!"
