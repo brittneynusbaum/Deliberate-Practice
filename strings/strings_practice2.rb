8c18529be7422438b8122e7913ff7355e2a17446
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 20:44:22 -0700

    adds practice 2 solution

--- strings/strings_practice2.rb
+++ strings/strings_practice2.rb
@@ -1,2 +1,6 @@
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
+first_name = "Brittney"
+last_name = "Nusbaum"
+
+p "My name is #{first_name} #{last_name}"
