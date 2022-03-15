d9a0ae687c32dfa3f60dde27df9ad79642bd9a2b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 20:57:25 -0700

    adds practice 6 solution

--- strings/strings_practice6.rb
+++ strings/strings_practice6.rb
@@ -1,1 +1,8 @@
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+p "Enter a name"
+
+user_input = gets.chomp.downcase
+if user_input != "santa"
+  p "You're not Santa!"
+end
