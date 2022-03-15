6b8d711c7d3abf00a5abf3819e9a65ca17561007
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 21:11:07 -0700

    adds practice 8 solution

--- strings/strings_practice8.rb
+++ strings/strings_practice8.rb
@@ -1,1 +1,8 @@
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+p "Enter a book title and author"
+
+book_title = gets.chomp
+author = gets.chomp
+
+p "The book title is #{book_title} and the author is #{author}!"
