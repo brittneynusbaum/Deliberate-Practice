123e6209bf2e67f7f14694cbcce446fbe58a8f3a
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 21:15:54 -0700

    adds practice 10 solution

--- strings/strings_practice10.rb
+++ strings/strings_practice10.rb
@@ -1,1 +1,9 @@
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+p "Enter three cities, one at a time:"
+
+user_city1 = gets.chomp
+user_city2 = gets.chomp
+user_city3 = gets.chomp
+
+p "Your cities are " + user_city1 + ", " + user_city2 + ", and " + user_city3 + "!"
