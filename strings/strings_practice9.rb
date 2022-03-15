e8b78b5b636e2b15596ce281691bd97cd2b7e916
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-14 21:13:11 -0700

    adds practice 9 solution

--- strings/strings_practice9.rb
+++ strings/strings_practice9.rb
@@ -1,1 +1,10 @@
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+p "Enter a password"
+user_password = gets.chomp.capitalize
+
+if user_password == "Joshua"
+  p "Shall we play a game?"
+else
+  p "Access denied"
+end
