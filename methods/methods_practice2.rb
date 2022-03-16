70a6fd4e72603f9bda2fe199db3884f5309023b0
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 21:43:01 -0700

    adds methods practice 2 solution

--- methods/methods_practice2.rb
+++ methods/methods_practice2.rb
@@ -1,1 +1,8 @@
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Enter a number: "
+user_unput = gets.chomp.to_i 
+
+if user_unput > 100
+  puts "That's a big number!"
+end
