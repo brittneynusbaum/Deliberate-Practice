92c9466e2ee6bac33807f07009f1611d1fc5fbca
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 22:00:51 -0700

    adds methods practice 10 solution

--- methods/methods_practice10.rb
+++ methods/methods_practice10.rb
@@ -1,1 +1,8 @@
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+puts "Enter a word below: "
+
+user_input = gets.chomp
+if user_input.length > 5
+  puts "That's a long word!"
+end
