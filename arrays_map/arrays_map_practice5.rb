eb600094dc7faf8787049947932b9987737e603a
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 19:51:25 -0700

    adds practice 5 round 2

--- arrays_map/arrays_map_practice5.rb
+++ arrays_map/arrays_map_practice5.rb
@@ -1,14 +1,14 @@
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
+strings = ["hello", "goodbye"]
+length = []
 
+strings.each do |string|
+  length << string.length
+end
 
-
-
-
-
-
-
+p length
 
 
 
@@ -20,9 +20,9 @@
 
 ## Round 1 ##
 
-strings = ["hello", "goodbye"]
-strings_length = []
-strings.each do |string|
-  strings_length << string.length
-end
-p strings_length
+# strings = ["hello", "goodbye"]
+# strings_length = []
+# strings.each do |string|
+#   strings_length << string.length
+# end
+# p strings_length
