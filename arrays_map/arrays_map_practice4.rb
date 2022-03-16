3a1a1ee86756516433493a4ce16c04410714ac8b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 19:48:24 -0700

    adds practice 4 round 2

--- arrays_map/arrays_map_practice4.rb
+++ arrays_map/arrays_map_practice4.rb
@@ -1,14 +1,14 @@
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
+numbers = [1, 2, 3]
+sum = []
 
+numbers.each do |number|
+  sum << number + 7
+end
 
-
-
-
-
-
-
+p sum
 
 
 
@@ -20,11 +20,11 @@
 
 ## Round 1 ##
 
-numbers = [1, 2, 3]
-plus_seven = []
+# numbers = [1, 2, 3]
+# plus_seven = []
 
-numbers.each do |number|
-  plus_seven << number + 7
-end
+# numbers.each do |number|
+#   plus_seven << number + 7
+# end
 
-p plus_seven
+# p plus_seven
