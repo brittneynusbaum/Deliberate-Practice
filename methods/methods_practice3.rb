f5f6cc19b973cf57fe2e7b7c98bcbf55d618a491
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 21:45:17 -0700

    adds methods practice 3 solution

--- methods/methods_practice3.rb
+++ methods/methods_practice3.rb
@@ -1,1 +1,8 @@
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+puts "Enter two numbers, one at a time: "
+
+user_number1 = gets.chomp.to_i
+user_number2 = gets.chomp.to_i
+
+puts user_number1 + user_number2
