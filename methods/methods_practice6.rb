d6d5fa04143f816dc25edbb2a9a1d1811653c2fd
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 21:53:11 -0700

    adds methods practice 6 solution

--- methods/methods_practice6.rb
+++ methods/methods_practice6.rb
@@ -1,1 +1,8 @@
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+puts "Enter two words, one at a time: "
+
+user_input1 = gets.chomp
+user_input2 = gets.chomp
+
+puts "#{user_input1} #{user_input2}"
