46beb3febd58d13de87b8b5211b5b77fa2cf6ebf
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 21:54:24 -0700

    adds methods practice 7 solution

--- methods/methods_practice7.rb
+++ methods/methods_practice7.rb
@@ -1,1 +1,6 @@
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+puts "Enter a word below: "
+
+user_input = gets.chomp
+puts user_input.length 
