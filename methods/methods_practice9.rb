65fdd4a54859857f0eb983597d5a0dcfbdf22876
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-15 21:58:39 -0700

    adds methods practice 9 solution

--- methods/methods_practice9.rb
+++ methods/methods_practice9.rb
@@ -1,1 +1,7 @@
 # 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+puts "Enter two numbers, one at a time: "
+
+user_input1 = gets.chomp.to_i
+user_input2 = gets.chomp.to_i
+puts user_input1 * user_input2
