00e8a5d8b0a0928abec9fb779efb7b3acc4122d3
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 16:24:24 -0700

    adds practice 6 solution

--- array_select/arrays_select_practice6.rb
+++ array_select/arrays_select_practice6.rb
@@ -1,2 +1,13 @@
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+shorter = []
+
+items.each do |item|
+  if item[:name].length < 6
+    shorter << item
+  end
+end
+
+p shorter
