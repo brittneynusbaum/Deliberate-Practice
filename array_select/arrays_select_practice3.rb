aaa8707d8d96184f56acc67f422606e2ede161b4
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 14:35:22 -0700

    adds practice 3 solution

--- array_select/arrays_select_practice3.rb
+++ array_select/arrays_select_practice3.rb
@@ -1,2 +1,14 @@
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+greater_than_five = []
+
+items.each do |item|
+  if item[:price] > 5
+    greater_than_five << item
+  end
+end
+
+p greater_than_five
