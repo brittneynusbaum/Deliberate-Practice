ab7a2785a5b3288c8db013082b08fe698dbed215
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 16:21:33 -0700

    adds practice 5 solution

--- array_select/arrays_select_practice5.rb
+++ array_select/arrays_select_practice5.rb
@@ -1,2 +1,13 @@
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+shorter = []
+
+strings.each do |string|
+  if string.length < 4
+    shorter << string
+  end
+end
+
+p shorter
