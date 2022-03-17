8c0b2d06b48432a25bd23ca58993886229d05ab2
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 16:34:34 -0700

    adds practice 9 solution

--- array_select/arrays_select_practice9.rb
+++ array_select/arrays_select_practice9.rb
@@ -1,2 +1,12 @@
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+less_ten = []
+
+items.each do |item|
+  if item[:price] < 10
+    less_ten << item
+  end 
+end
+p less_ten
