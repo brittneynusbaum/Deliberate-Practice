51ade63ddbbf7da6b60f465de8023a7315e3c841
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 16:28:54 -0700

    adds practice 7 solution

--- array_select/arrays_select_practice7.rb
+++ array_select/arrays_select_practice7.rb
@@ -1,2 +1,13 @@
-#  7. Start with an array of numbers and create a new array with only the numbers less than 10.
-#     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+#  7. Start with an array of numbers and create a new array with only the numbers greater than or equal to 23.
+#     For example, [8, 23, 0, 44, 1980, 3] becomes [23, 44, 1980].
+
+numbers = [8, 23, 0, 44, 1980, 3]
+larger = []
+
+numbers.each do |number|
+  if number >= 23
+    larger << number
+  end
+end
+
+p larger
