9c4e969a747c928ae4dae879d9ce18e9b70471dd
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 16:19:17 -0700

    adds practice 4 solution

--- array_select/arrays_select_practice4.rb
+++ array_select/arrays_select_practice4.rb
@@ -1,2 +1,13 @@
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+even = []
+
+numbers.each do |number|
+  if number % 2 ==0
+    even << number
+  end 
+end
+
+p even
