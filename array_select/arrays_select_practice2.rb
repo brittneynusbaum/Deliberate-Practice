589e916446b2bf23b6b880a32004db03070b834e
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-17 14:26:38 -0700

    adds practice 2 solution

--- array_select/arrays_select_practice2.rb
+++ array_select/arrays_select_practice2.rb
@@ -1,2 +1,13 @@
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+strings = ["winner", "winner", "chicken", "dinner"]
+start_with_w = []
+
+strings.each do |string|
+  if string[0] == "w"
+    start_with_w << string
+  end
+end
+
+p start_with_w
