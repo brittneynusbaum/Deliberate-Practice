72311539ef511af065c6321c4988a77a4e604cdd
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-18 19:40:01 -0700

    adds practice 3 solution

--- loops/loops_practice3.rb
+++ loops/loops_practice3.rb
@@ -1,1 +1,10 @@
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+p "enter a word, enter stop when done"
+
+user_input = gets.chomp.downcase
+i = 0
+while user_input != "stop"
+  user_input = gets.chomp.downcase
+end
+  
