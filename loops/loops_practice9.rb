5b73aca9e3ea088396db90f5407b60c0cf7fbd95
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-18 19:52:53 -0700

    adds practice 9 solution

--- loops/loops_practice9.rb
+++ loops/loops_practice9.rb
@@ -1,1 +1,8 @@
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+p "enter a word: "
+user_input = gets.chomp
+
+while user_input.length < 5
+  user_input = gets.chomp
+end
