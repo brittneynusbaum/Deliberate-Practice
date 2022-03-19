4352730a04387b7e26a5e9d763347eb17d6e894e
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-18 19:48:04 -0700

    adds practice 6 solution

--- loops/loops_practice6.rb
+++ loops/loops_practice6.rb
@@ -1,1 +1,8 @@
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+p "enter a number!"
+user_input = gets.chomp.to_i
+
+while user_input < 10
+  user_input = gets.chomp.to_i
+end
