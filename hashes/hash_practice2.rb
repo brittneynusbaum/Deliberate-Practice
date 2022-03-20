bc1cda8fb9fbf2786f2fec9626b6037f951d50f3
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:36:37 -0700

    adds practice 2 solution

--- hashes/hash_practice2.rb
+++ hashes/hash_practice2.rb
@@ -1,1 +1,18 @@
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+person1 = {
+  first_name: "Jessie",
+  last_name: "Stephens"
+}
+
+person2 = {
+  first_name: "Woody",
+  last_name: "Clark"
+}
+
+person3 = { 
+  first_name: "Annie",
+  last_name: "Barns"
+}
+
+p person1
