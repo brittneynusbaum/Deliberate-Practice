962a230b82192b210191c490b1286ed6f3c1dc6d
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:20:09 -0700

    adds practice 9 solution

--- arrays/arrays_practice9.rb
+++ arrays/arrays_practice9.rb
@@ -1,1 +1,9 @@
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1, 2]
+
+i = 0
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end
