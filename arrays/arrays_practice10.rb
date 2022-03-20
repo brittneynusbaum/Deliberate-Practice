447fcd4c491924db5591a9e7eca58301be45ca07
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:22:29 -0700

    adds practice 10 solution

--- arrays/arrays_practice10.rb
+++ arrays/arrays_practice10.rb
@@ -1,1 +1,6 @@
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+countries = ["Canada", "Germany", "Finland"]
+countries << "United States"
+
+p countries
