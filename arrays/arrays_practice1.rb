4e33d17943525b902fdcd5979f18547fe64357a5
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 20:50:41 -0700

    adds practice 1 solution

--- arrays/arrays_practice1.rb
+++ arrays/arrays_practice1.rb
@@ -1,1 +1,6 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+words = ["these", "are", "words"]
+words << "to" 
+words << "test"
+p words
