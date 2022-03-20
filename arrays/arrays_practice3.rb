7a10bf17fd6efd6f0dc5f49fdf58cade644208c7
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:04:31 -0700

    adds practice 3 solution

--- arrays/arrays_practice3.rb
+++ arrays/arrays_practice3.rb
@@ -1,1 +1,9 @@
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1, 2, 3, 4, 5]
+
+i = 0
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end
