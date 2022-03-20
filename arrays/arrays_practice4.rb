f52b8be61278d9b1222db3222e33ea682a25888c
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:06:07 -0700

    adds practice 4 solution

--- arrays/arrays_practice4.rb
+++ arrays/arrays_practice4.rb
@@ -1,1 +1,9 @@
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+numbers = [1]
+
+numbers << 2
+numbers << 3
+numbers << 4
+
+p numbers
