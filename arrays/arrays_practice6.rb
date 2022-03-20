363113fbaa2209adaee04000c955cca152a7b44f
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:15:45 -0700

    adds practice 6 solution

--- arrays/arrays_practice6.rb
+++ arrays/arrays_practice6.rb
@@ -1,1 +1,9 @@
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+names = ["brittney", "melanie", "rebecca"]
+
+i = 0
+while i < names.length
+  p names[i]
+  i += 1
+end
