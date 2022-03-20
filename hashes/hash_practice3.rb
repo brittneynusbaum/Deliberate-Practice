3cef31435999422ba6a78c4cbe7e72f0e8ddb86d
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:41:21 -0700

    adds practice 3 solution

--- hashes/hash_practice3.rb
+++ hashes/hash_practice3.rb
@@ -1,1 +1,11 @@
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+items = {
+  pancakes: 10.99,
+  crepes: 15.00,
+  scrambled_eggs: 5.00
+}
+
+items["french_toast"] = 13.99
+
+p items
