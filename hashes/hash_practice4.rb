ba149d39a355197defca1ed5e1fc6609aa8eadc2
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:44:02 -0700

    adds practice 4 solution

--- hashes/hash_practice4.rb
+++ hashes/hash_practice4.rb
@@ -1,1 +1,13 @@
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+books = {
+  title: "The Hobbit",
+  author: "J.R.R. Tolkein",
+  pages: 310,
+  language: "English"
+}
+
+p books[:title]
+p books[:author]
+p books[:pages]
+p books[:language]
