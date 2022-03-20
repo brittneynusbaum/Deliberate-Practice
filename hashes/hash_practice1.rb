d374da9ee5c625c5f07c07b6e8c026e266f07d7a
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-19 23:33:16 -0700

    adds practice 1 solution

--- hashes/hash_practice1.rb
+++ hashes/hash_practice1.rb
@@ -1,1 +1,11 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+person = {
+  first_name: "ollie",
+  last_name: "smith",
+  email: "olliesmith@cat.com"
+}
+
+p person[:first_name]
+p person[:last_name]
+p person[:email]
