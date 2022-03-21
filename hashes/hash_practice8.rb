4eeaac3c7830d69450cc7b3a84b216c0e672a11b
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-20 23:14:28 -0700

    adds practice 8 solution

--- hashes/hash_practice8.rb
+++ hashes/hash_practice8.rb
@@ -1,1 +1,9 @@
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+laptops = [
+  {brand: "Apple", model: "Macbook Pro"},
+  {brand: "Microsoft", model: "Microsoft Surface Laptop Studio"},
+  {brand: "HP", model: "HP Spectre "}
+]
+
+p laptops[1][:model]
