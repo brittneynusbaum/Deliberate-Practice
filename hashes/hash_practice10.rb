18858cf45655dc667a24e30439e09c3f4d5809b3
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-20 23:22:35 -0700

    adds practice 9 solution

--- hashes/hash_practice10.rb
+++ hashes/hash_practice10.rb
@@ -1,1 +1,2 @@
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
--- hashes/hash_practice9.rb
+++ hashes/hash_practice9.rb
@@ -1,1 +1,6 @@
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+definitions = {"test" => "the trial of the quality of something:", "practice" => "habitual"}
+definitions["school"] = "an institution for instruction in a particular skill or field."
+
+p definitions
