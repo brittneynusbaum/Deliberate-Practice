5a404b09b463548c6b0c8d68d4a2e424683388c1
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-20 23:23:00 -0700

    adds practice 10 solution

--- hashes/hash_practice10.rb
+++ hashes/hash_practice10.rb
@@ -1,2 +1,10 @@
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
-
+shirt = {
+  brand: "Madewell",
+  color: "Blue",
+  size: "Medium"
+  }
+  
+p shirt[:brand]
+p shirt[:color]
+p shirt[:size]
