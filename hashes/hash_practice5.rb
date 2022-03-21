9fa2597008b7b73802e9e62af7379ccc6daff8ec
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-20 22:44:48 -0700

    adds practice 5 solution

--- hashes/hash_practice5.rb
+++ hashes/hash_practice5.rb
@@ -1,1 +1,9 @@
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+authors = [
+  {title: "In a Dark Dark Wood", author: "Ruth Ware"},
+  {title: "The Hobbit", author: "J.R.R. Tolkein"},
+  {title: "Annihilation", author: "Jeff VanderMeer"}
+]
+
+p authors[2][:author]
