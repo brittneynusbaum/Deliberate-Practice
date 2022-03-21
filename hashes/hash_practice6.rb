19b5855edb3b2dd8542f2574012a21d1d88544bb
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-20 23:05:44 -0700

    adds practice 6 solution

--- hashes/hash_practice6.rb
+++ hashes/hash_practice6.rb
@@ -1,1 +1,11 @@
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+captitals = {
+  "California" => "Sacramento",
+  "Colorado" => "Denver",
+  "Oregon" => "Portland"
+}
+
+captitals["Washington"] = "Seattle"
+
+p captitals
