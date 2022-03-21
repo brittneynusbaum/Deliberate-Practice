2dfdb6ff090cd2cbe36c2048825529af0c9253b3
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-20 23:08:06 -0700

    adds practice 7 solution

--- hashes/hash_practice7.rb
+++ hashes/hash_practice7.rb
@@ -1,1 +1,11 @@
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+laptop = {
+  brand: "Apple",
+  model: "Macbook Pro",
+  year: "2021"
+}
+
+p laptop[:brand]
+p laptop[:model]
+p laptop[:year]
