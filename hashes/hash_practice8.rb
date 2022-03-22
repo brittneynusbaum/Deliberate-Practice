253da2fa688eb1939258bee925fb21aab617c8bc
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-21 23:00:16 -0700

    adds round 2 practice 8 solution

--- hashes/hash_practice8.rb
+++ hashes/hash_practice8.rb
@@ -3,7 +3,13 @@
 ## Round 2 ##
 
 
+laptops = [
+  {brand: "Apple", model: "Macbook Pro"},
+  {brand: "Microsoft", model: "Microsoft Surface Laptop Studio"},
+  {brand: "HP", model: "HP Spectre"}
+]
 
+p laptops[1][:model]
 
 
 
