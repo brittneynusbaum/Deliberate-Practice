956e74f16774e2d6c71afd591c4f31d8e1980f70
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-12 23:25:20 -0700

    adds practice 2 solution

--- arrays_reduce/arrays_reduce_practice.rb
+++ arrays_reduce/arrays_reduce_practice.rb
@@ -1,13 +1,25 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 26.
 
+# numbers = [5, 10, 8, 3]
+# sum = 0
+# numbers.each do |number|
+#   sum += number
+# end
 
+# p sum
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
 
+sports = ["volleyball", "basketball", "badminton"]
+sports = sports.join
+p sports
+
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+
 
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
